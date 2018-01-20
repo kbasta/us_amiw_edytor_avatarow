6230e3bf310bb2cde70e0353801b6b396ad67c09
kbasta <k_basta@hotmail.com>
2018-01-20 21:56:00 +0100

    Implementacja biblioteki jQuery, dostosowanie funkcji

--- scripts/options.js
+++ scripts/options.js
@@ -1,58 +1,12 @@
-window.onload = function() {
-    var optHair = document.getElementById("hair");
-    var optGlasses = document.getElementById("glasses");
-    var optShirt = document.getElementById("shirt");
-    var optTrousers = document.getElementById("trousers");
-    
-    optHair.onclick = function() {
-        var avatarHair = document.getElementById("avatar-hair");
-        var span = optHair.getElementsByTagName("span")[0];
-
-        if (avatarHair.style.display !== "none") {
-            avatarHair.style.display = "none";
-            span.innerHTML = "dodaj";
+$(function() {
+    $("#options ul").on("click", "li", function(event) {
+        var avatarId = "#avatar-" + event.currentTarget.id;
+        $(avatarId).toggle();
+        var span = $(event.currentTarget).find("span");
+        if ($(avatarId).is(":visible")) {
+            span.text("usun");
         } else {
-            avatarHair.style.display = "block";
-            span.innerHTML = "usun";
+            span.text("dodaj");
         }
-    }
-
-    optGlasses.onclick = function() {
-        var avatarGlasses = document.getElementById("avatar-glasses");
-        var span = optGlasses.getElementsByTagName("span")[0];
-
-        if (avatarGlasses.style.display !== "none") {
-            avatarGlasses.style.display = "none";
-            span.innerHTML = "dodaj";
-        } else {
-            avatarGlasses.style.display = "block";
-            span.innerHTML = "usun";
-        }
-    }
-
-    optShirt.onclick = function() {
-        var avatarShirt = document.getElementById("avatar-shirt");
-        var span = optShirt.getElementsByTagName("span")[0];
-
-        if (avatarShirt.style.display !== "none") {
-            avatarShirt.style.display = "none";
-            span.innerHTML = "dodaj";
-        } else {
-            avatarShirt.style.display = "block";
-            span.innerHTML = "usun";
-        }
-    }
-
-    optTrousers.onclick = function() {
-        var avatarTrousers = document.getElementById("avatar-trousers");
-        var span = optTrousers.getElementsByTagName("span")[0];
-
-        if (avatarTrousers.style.display !== "none") {
-            avatarTrousers.style.display = "none";
-            span.innerHTML = "dodaj";
-        } else {
-            avatarTrousers.style.display = "block";
-            span.innerHTML = "usun";
-        }
-    }
-}   
+    });
+});
